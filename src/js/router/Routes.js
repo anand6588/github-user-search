6df249ba19d4efbf930141ce2f28133c7155e4d1
Anand Thyagaraju <anand.thyagaraju@a-cti.com>
2017-03-13 11:49:04 +0800

    added .htaccess file

--- src/js/router/Routes.js
+++ src/js/router/Routes.js
@@ -5,7 +5,6 @@ import Home from '../component/Home.jsx';
 import SearchResult from '../containers/SearchContainer';
 import User from '../containers/User.js';
 
-// Define routes
 export default (
 		<Route path="/">
 			<IndexRoute component={Home} />
